ad4ee892502211a03c013b5cc13719b5445a3d56
Gerardo Castro <gerardo.castro.torres@gmail.com>
2014-03-05 10:57:37 -0600

    send error messages to client

--- lib/devise/oauth2_providable/strategies/oauth2_grant_type_strategy.rb
+++ lib/devise/oauth2_providable/strategies/oauth2_grant_type_strategy.rb
@@ -28,7 +28,7 @@ module Devise
 
       def oauth_error!(error_code = :invalid_request)
         body = {:error => error_code}
-        body[:error_description] = I18n.t("devise.fail.#{error_code.to_s}")
+        body[:error_description] = I18n.t("devise.failure.#{error_code.to_s}")
         custom! [401, {'Content-Type' => 'application/json'}, [body.to_json]]
         throw :warden
       end
